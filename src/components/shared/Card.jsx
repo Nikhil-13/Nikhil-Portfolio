2d10a9a6cf127b01b45a2ee6c8cea7d3db12ca18
Nikhil-13 <nnik270@gmail.com>
2023-07-20 12:44:27 +0530

    changes

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,5 +1,37 @@
 function Card() {
-	return <div>Card</div>
+	return (
+		<div className='work-card'>
+			<div>
+				<h4 className='company-name'>
+					<a href='' target='_blank' rel='noopener noreferrer'>
+						Moka Financial Technologies
+					</a>
+				</h4>
+				<span className='wrok-duration' style={{ opacity: '.7' }}>
+					11/2020-12-2022
+				</span>
+			</div>
+			<p className='job-role' style={{ fontSize: '1.25rem' }}>
+				Full Stack Developer{' '}
+				<span className='job-location' style={{ fontSize: '1.025rem', opacity: '.8' }}>
+					Montreal, Canada
+				</span>
+			</p>
+			<p className='job-description'>
+				Worked on the Moka mobile application building crucial features related to KYC and
+				money movement on both the front-end and back-end. I was also able to teach more
+				advanced TypeScript to my colleagues, through PR reviews and "lunch &amp; learns".
+			</p>
+			<hr style={{ opacity: '.5' }} />
+			<div className='tech-stack'>
+				<span className='tech-stack-item'>React Native</span>
+				<span className='tech-stack-item'>TypeScript</span>
+				<span className='tech-stack-item'>Node.js</span>
+				<span className='tech-stack-item'>MongoDB</span>
+				<span className='tech-stack-item'>GraphQL</span>
+			</div>
+		</div>
+	)
 }
 
 export default Card
--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,8 +1,14 @@
+import Card from '../components/shared/Card'
 function Work() {
 	return (
 		<section id='work'>
 			<h1>Work</h1>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, tenetur!</p>
+			<div className='card-wrapper'>
+				<Card />
+				<Card />
+				<Card />
+				<Card />
+			</div>
 		</section>
 	)
 }
