47a2ca473098ff397dae8906191bd8fecf664d56
Nikhil-13 <nnik270@gmail.com>
2023-07-27 14:17:09 +0530

    added contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,26 +1,13 @@
 import { useForm } from 'react-hook-form'
+import ContactForm from '../components/ContactForm'
 
 function Contact() {
-	const { register, handleSubmit } = useForm()
-	const onSubmit = (data) => console.log(data)
 	return (
 		<section id='contact'>
 			<h1>Contact</h1>
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<input
-					type='text'
-					placeholder='First name'
-					{...register('First name', { required: true, maxLength: 80 })}
-				/>
-				<input
-					type='text'
-					placeholder='Email'
-					{...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
-				/>
-				<textarea {...register} />
-
-				<input type='submit' />
-			</form>
+			<div className='form-container'>
+				<ContactForm />
+			</div>
 		</section>
 	)
 }
