4418ef97d32dda514a83dc2a2ff5b4d5edcfcff1
Nikhil-13 <nnik270@gmail.com>
2023-07-28 16:00:34 +0530

    made some validation changes form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,54 @@
 import { useForm } from 'react-hook-form'
-import ContactForm from '../components/ContactForm'
 
 function Contact() {
+	console.count()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm()
+	const onSubmit = (data) => {
+		console.log(data)
+	}
+
 	return (
 		<section id='contact'>
 			<h1>Contact</h1>
 			<div className='form-container'>
-				<ContactForm />
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<input
+						type='text'
+						placeholder='Name'
+						{...register('Name', { required: true, maxLength: 80 })}
+						style={errors.Name && { borderColor: 'red' }}
+					/>
+					<input
+						type='text'
+						placeholder='Email'
+						{...register('Email', { required: true, pattern: /^\S+@\S+$/i })}
+						style={errors.Email && { borderColor: 'red' }}
+					/>
+					<input
+						type='text'
+						placeholder='Subject'
+						{...register('Subject', { required: true, maxLength: 30 })}
+						style={errors.Subject && { borderColor: 'red' }}
+					/>
+
+					<textarea
+						placeholder='Write your message here...'
+						{...register('Message', {
+							required: true,
+							max: 200,
+							min: 50,
+							maxLength: 300,
+						})}
+					/>
+					<input
+						type='submit'
+						disabled={Object.keys(errors).length === 0 ? false : true}
+					/>
+				</form>
 			</div>
 		</section>
 	)
