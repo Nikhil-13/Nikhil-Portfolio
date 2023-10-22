fef315a7c69973b68dce6c528b520be498d207f6
Nikhil-13 <nnik270@gmail.com>
2023-10-22 23:46:15 +0530

    changes

--- src/data/data.js
+++ src/data/data.js
@@ -1,22 +1,22 @@
 export const work = [
-	{
-		id: 1,
-		company: 'ABC',
-		companyUrl: 'http://google.com',
-		location: 'Panchkula,India',
-		role: 'Fullstack Dev',
-		years: '2011-2012',
-		description: 'The quick brown fox jumps over the lazy dog.',
-		stack: ['React', 'TypeScript', 'Node.js', 'MongoDB', 'GraphQL'],
-	},
-	{
-		id: 2,
-		company: 'XYZ',
-		companyUrl: 'http://google.com',
-		location: 'Panchkula,India',
-		role: 'Fullstack Dev',
-		years: '2011-2012',
-		description: 'The quick brown fox jumps over the lazy dog.',
-		stack: ['React', 'TypeScript', 'Node.js', 'MongoDB', 'GraphQL'],
-	},
-]
+  {
+    id: 1,
+    company: 'IT-Waves',
+    companyUrl: 'http://google.com',
+    location: 'Chandigarh,India',
+    role: 'React Native Intern',
+    years: 'Sept-2023 -',
+    description: 'The quick brown fox jumps over the lazy dog.',
+    stack: ['React', 'React-Native', 'HTML', 'CSS', 'JS'],
+  },
+  {
+    id: 2,
+    company: 'XYZ',
+    companyUrl: 'http://google.com',
+    location: 'Panchkula,India',
+    role: 'Fullstack Dev',
+    years: '2011-2012',
+    description: 'The quick brown fox jumps over the lazy dog.',
+    stack: ['React', 'TypeScript', 'Node.js', 'MongoDB', 'GraphQL'],
+  },
+];
